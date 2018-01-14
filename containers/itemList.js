fa7e0503efc09423b24d7d2c75e04b6fea1d512d
Romina Martin <rominamartin1d@gmail.com>
2018-01-14 14:25:31 +0100

    Pull to refresh added

--- containers/itemList.js
+++ containers/itemList.js
@@ -10,13 +10,22 @@ import { parseOnionData } from '../utils/parsers';
 export default class ItemList extends Component {
     constructor(props) {
         super(props);
-        this.state = { data: [] };
+        this.state = { data: [], refreshing: false};
+        this.onRefresh = this.onRefresh.bind(this);
     }
 
     componentDidMount() {
         var self = this;
         getServerData().then(function(data) {
-            self.setState({data: parseOnionData(data)});
+            self.setState({...self.state, data: parseOnionData(data)});
+        }).catch(function(e) { console.log(e)});
+    }
+
+    onRefresh() {
+        let self = this;
+        self.setState({...this.state, refreshing: true});
+        getServerData().then(function(data) {
+            self.setState({...self.state, data: parseOnionData(data), refreshing: false});
         }).catch(function(e) { console.log(e)});
     }
 
@@ -28,6 +37,8 @@ export default class ItemList extends Component {
                     <Item title={item.title} id={item.key}></Item>
                 )}
                 keyExtractor={(item, key) => key}
+                onRefresh={this.onRefresh}
+                refreshing={this.state.refreshing}
             />
         );
     }
